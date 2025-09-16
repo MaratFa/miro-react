7f47596af5efa81564266047288025f470ad8209
MaratFa <evolution99@rambler.ru>
2025-09-16 05:45:00 +0300

    edited

--- src/features/auth/register.page.tsx
+++ src/features/auth/register.page.tsx
@@ -1,5 +1,20 @@
+import { ROUTES } from "@/shared/model/routes";
+import { Link } from "react-router-dom";
+import { AuthLayout } from "./auth-layout";
+
 function RegisterPage() {
-  return <div>Register page</div>;
+  return (
+    <AuthLayout
+      title="Sign up"
+      description="Enter your email address and password to log in"
+      form={<form></form>}
+      footerText={
+        <>
+          Do you have an account? <Link to={ROUTES.LOGIN}>Log in</Link>
+        </>
+      }
+    />
+  );
 }
 
 export const Component = RegisterPage;
