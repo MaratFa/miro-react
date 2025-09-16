6ed0b660aa3b922872ee37b190424320adcb18e9
MaratFa <evolution99@rambler.ru>
2025-09-16 05:07:09 +0300

    edited

--- src/features/auth/login.page.tsx
+++ src/features/auth/login.page.tsx
@@ -1,53 +1,19 @@
-import { rqClient } from "@/shared/api/instance";
 import { ROUTES } from "@/shared/model/routes";
-import { Button } from "@/shared/ui/kit/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/shared/ui/kit/card";
 import { Link } from "react-router-dom";
 import { AuthLayout } from "./auth-layout";
 
 function LoginPage() {
   return (
-    <AuthLayout>
-
-
-
-
-
-
-
-
-
-    </AuthLayout>
-
-
-
-
-    // <main className="grow flex flex-col pt-[200px] items-center">
-    //   <Card className="w-full max-w-[400px]">
-    //     <CardHeader>
-    //       <CardTitle>Log in</CardTitle>
-    //       <CardDescription>
-    //         Enter your email address and password to log in
-    //       </CardDescription>
-    //     </CardHeader>
-    //     <CardContent></CardContent>
-    //     <CardFooter>
-    //       <p className="text-sm text-muted-foreground">
-    //         No account?{" "}
-    //         <Button asChild variant={'link'}>
-    //           <Link className="underline text-primary" to={ROUTES.REGISTER}>Sign up</Link>
-    //         </Button>
-    //       </p>
-    //     </CardFooter>
-    //   </Card>
-    // </main>
+    <AuthLayout
+      title="Log in"
+      description="Enter your email address and password to log in"
+      form={<form></form>}
+      footerText={
+        <>
+          No account? <Link to={ROUTES.REGISTER}>Sign up</Link>
+        </>
+      }
+    />
   );
 }
 
