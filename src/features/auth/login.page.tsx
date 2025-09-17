72aabfbae3127248aa5ad4411948f217df7e9f2d
MaratFa <evolution99@rambler.ru>
2025-09-17 05:33:06 +0300

    edited

--- src/features/auth/login.page.tsx
+++ src/features/auth/login.page.tsx
@@ -1,15 +1,15 @@
 import { ROUTES } from "@/shared/model/routes";
 import { Link } from "react-router-dom";
 import { AuthLayout } from "./auth-layout";
+import { LoginForm } from "./login-form";
 
 function LoginPage() {
-  console.log(ROUTES.REGISTER);
   
   return (
     <AuthLayout
       title="Log in"
       description="Enter your email address and password to log in"
-      form={<form></form>}
+      form={<LoginForm />}
       footerText={
         <>
           No account? <Link to={ROUTES.REGISTER}>Sign up</Link>
