a85003ae9c5e3e7ff54137a4e4193b6fd840ae59
MaratFa <evolution99@rambler.ru>
2025-09-25 21:13:19 +0300

    edited

--- src/app/protected-route.tsx
+++ src/app/protected-route.tsx
@@ -1,6 +1,8 @@
 import { ROUTES } from "@/shared/model/routes";
+import { Outlet, redirect } from "react-router-dom";
 import { useSession } from "@/shared/model/session";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { enableMocking } from "@/shared/api/mocks";
 
 export function ProtectedRoute() {
   const { session } = useSession();
@@ -10,4 +12,16 @@ export function ProtectedRoute() {
   }
 
   return <Outlet />;
+}
+
+export async function protectedLoader() {
+  await enableMocking();
+
+  const token = await useSession.getState().refreshToken();
+
+  if (!token) {
+    return redirect(ROUTES.LOGIN);
+  }
+
+  return null;
 }
