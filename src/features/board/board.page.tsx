647fd0513bf2e37ce89d71a68a9ecf97ef3d932a
MaratFa <evolution99@rambler.ru>
2025-10-01 23:51:39 +0300

    edited

--- src/features/board/board.page.tsx
+++ src/features/board/board.page.tsx
@@ -3,7 +3,36 @@ import { useParams } from "react-router-dom";
 
 function BoardPage() {
   const params = useParams<PathParams[typeof ROUTES.BOARD]>();
-  return <div>Board page {params.boardId}</div>;
+  return (
+    <Layout>
+      <Dots />
+    </Layout>
+  );
 }
 
 export const Component = BoardPage;
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="grow relative" tabIndex={0}>
+      {children}
+    </div>
+  );
+}
+
+function Dots() {
+  return (
+    <div className="absolute inset-0 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
+  );
+}
+
+function Canvas({
+  children,
+  ...props
+}: { children: React.ReactNode } & React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div {...props} className="absolute inset-0">
+      {children}
+    </div>
+  );
+}
