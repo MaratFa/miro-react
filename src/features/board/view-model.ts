016eebef06ce0b2f25e74769843d4b8344259108
MaratFa <evolution99@rambler.ru>
2025-10-07 06:18:26 +0300

    added

---
+++ src/features/board/view-model.ts
@@ -0,0 +1,88 @@
+import { useState } from "react";
+
+type AddStickerViewState = {
+  type: "add-sticker";
+};
+
+type IdleViewState = {
+  type: "idle";
+  selectedIds: string[];
+};
+
+type ViewState = AddStickerViewState | IdleViewState;
+
+export function useBoardViewState() {
+  const [viewState, setViewState] = useState<ViewState>({
+    type: "idle",
+    selectedIds: [],
+  });
+
+  const goToIdle = () => {
+    setViewState({
+      type: "idle",
+      selectedIds: [],
+    });
+  };
+
+  const selection = (
+    ids: string[],
+    modif: "replace" | "add" | "toggle" = "replace"
+  ) =>
+    setViewState((s) => {
+      if (s.type === "idle") {
+        return selectItems(s, ids, modif);
+      }
+
+      return s;
+    });
+
+  const goToAddSticker = () => {
+    setViewState({
+      type: "add-sticker",
+    });
+  };
+
+  return {
+    viewState,
+    selection,
+    goToIdle,
+    goToAddSticker,
+  };
+}
+
+function selectItems(
+  viewState: IdleViewState,
+  ids: string[],
+  modif: "replace" | "add" | "toggle" = "replace"
+) {
+  if (viewState.type === "idle") {
+    if (modif === "replace") {
+      return {
+        ...viewState,
+        selectedIds: ids,
+      };
+    }
+
+    if (modif === "add") {
+      return {
+        ...viewState,
+        selectedIds: Array.from(new Set([...viewState.selectedIds, ...ids])),
+      };
+    }
+
+    if (modif === "toggle") {
+      const currentIds = new Set(viewState.selectedIds);
+      const newIds = new Set(ids);
+
+      const base = viewState.selectedIds.filter((id) => !newIds.has(id));
+      const added = ids.filter((id) => !currentIds.has(id));
+
+      return {
+        ...viewState,
+        selectedIds: [...base, ...added],
+      };
+    }
+  }
+
+  return viewState;
+}
