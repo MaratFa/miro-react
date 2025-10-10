9a1faf5e45fac89360816b4f75610badb61fb819
MaratFa <evolution99@rambler.ru>
2025-10-10 04:50:26 +0300

    added

---
+++ src/features/board/view-model/variants/idle.ts
@@ -0,0 +1,36 @@
+import { IdleViewState } from "../../model/view-state";
+import { ViewModel, ViewModelParams } from "../use-view-model";
+
+export function useIdleViewModel({
+  nodesModel,
+  viewStateModel,
+}: ViewModelParams) {
+  return (idleState: IdleViewState): ViewModel => ({
+    nodes: nodesModel.nodes.map((node) => ({
+      ...node,
+      isSelected: idleState.selectedIds.has(node.id),
+      onClick: (e) => {
+        if (e.ctrlKey || e.shiftKey) {
+          viewStateModel.selection([node.id], "toggle");
+        } else {
+          viewStateModel.selection([node.id], "replace");
+        }
+      },
+    })),
+    layot: {
+      onKeyDown: (e) => {
+        if (e.key === "s") {
+          viewStateModel.goToAddSticker();
+        }
+      },
+    },
+    actions: {
+      addSticker: {
+        isActive: false,
+        onClick: () => {
+          viewStateModel.goToAddSticker();
+        },
+      },
+    },
+  });
+}
