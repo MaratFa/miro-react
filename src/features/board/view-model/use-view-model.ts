4ab5f20ab9b7108e1a2a6fa0d7310dbf6b58ecc8
MaratFa <evolution99@rambler.ru>
2025-10-15 06:51:59 +0300

    edited

--- src/features/board/view-model/use-view-model.ts
+++ src/features/board/view-model/use-view-model.ts
@@ -1,10 +1,20 @@
 import { useState } from "react";
-import { AddStickerViewState, useAddStickerViewModel } from "./variants/add-sticker";
+import {
+  AddStickerViewState,
+  useAddStickerViewModel,
+} from "./variants/add-sticker";
 import { IdleViewState, useIdleViewModel } from "./variants/idle";
 import { ViewModelParams } from "./view-model-params";
 import { ViewModel } from "./view-model-type";
+import {
+  SelectionWindowViewState,
+  useSelectionWindowWiewModel,
+} from "./variants/selection-window";
 
-export type ViewState = AddStickerViewState | IdleViewState;
+export type ViewState =
+  | AddStickerViewState
+  | IdleViewState
+  | SelectionWindowViewState;
 
 export function useViewModel(params: Omit<ViewModelParams, "setViewState">) {
   const [viewState, setViewState] = useState<ViewState>({
@@ -15,10 +25,11 @@ export function useViewModel(params: Omit<ViewModelParams, "setViewState">) {
   const newParams = {
     ...params,
     setViewState,
-  }
+  };
 
   const addStickerViewModel = useAddStickerViewModel(newParams);
   const idleViewModel = useIdleViewModel(newParams);
+  const selectionWindowViewModel = useSelectionWindowWiewModel(newParams);
 
   let viewModel: ViewModel;
   switch (viewState.type) {
@@ -27,6 +38,10 @@ export function useViewModel(params: Omit<ViewModelParams, "setViewState">) {
       break;
     case "idle": {
       viewModel = idleViewModel(viewState);
+      break;
+    }
+    case "selection-window": {
+      viewModel = selectionWindowViewModel(viewState);
       break;
     }
     default:
