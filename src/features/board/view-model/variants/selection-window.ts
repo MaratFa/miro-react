d2a68c8a871d157e9fbf42103ce0d3287df462e7
MaratFa <evolution99@rambler.ru>
2025-10-16 05:17:33 +0300

    edited

--- src/features/board/view-model/variants/selection-window.ts
+++ src/features/board/view-model/variants/selection-window.ts
@@ -1,18 +1,15 @@
-import { createRectFromPoints } from "../../domain/rect";
+import { Point } from "../../domain/points";
+import { createRectFromPoints, isPointInRect } from "../../domain/rect";
+import { pointOnScreenToCanvas } from "../../domain/screen-to-canvas";
+import { selectItems } from "../../domain/selection";
 import { ViewModelParams } from "../view-model-params";
 import { ViewModel } from "../view-model-type";
 import { goToIdle } from "./idle";
 
 export type SelectionWindowViewState = {
   type: "selection-window";
-  startPoint: {
-    x: number;
-    y: number;
-  };
-  endPoint: {
-    x: number;
-    y: number;
-  };
+  startPoint: Point;
+  endPoint: Point;
 };
 
 export function useSelectionWindowWiewModel({
@@ -24,10 +21,34 @@ export function useSelectionWindowWiewModel({
     const rect = createRectFromPoints(state.startPoint, state.endPoint);
     return {
       selectionWindow: rect,
-      nodes: nodesModel.nodes,
+      nodes: nodesModel.nodes.map((node) => ({
+        ...node,
+        isSelected: isPointInRect(node, rect),
+      })),
       window: {
+        onMouseMove: (e) => {
+          const currentPoint = pointOnScreenToCanvas(
+            {
+              x: e.clientX,
+              y: e.clientY,
+            },
+            canvasRect
+          );
+          setViewState({
+            ...state,
+            endPoint: currentPoint,
+          });
+        },
         onMouseUp: () => {
-          setViewState(goToIdle());
+          const nodesIdsInRect = nodesModel.nodes
+            .filter((node) => isPointInRect(node, rect))
+            .map((node) => node.id);
+
+          setViewState(
+            goToIdle({
+              selectedIds: selectItems(new Set(), nodesIdsInRect, "replace"),
+            })
+          );
         },
       },
     };
