8e8e9c409c332d24e6fbd1b7d4f1744935038233
MaratFa <evolution99@rambler.ru>
2025-10-22 00:20:28 +0300

    edited

--- src/features/board/view-model/view-model-params.ts
+++ src/features/board/view-model/view-model-params.ts
@@ -2,11 +2,11 @@ import { Dispatch, SetStateAction } from "react";
 import { CanvasRect } from "../hooks/use-canvas-rect";
 import { NodesModel } from "../model/nodes";
 import { ViewState } from "./use-view-model";
-import { NodesRectsMap } from "../hooks/use-nodes-rects";
+import { NodesDimensionsMap } from "../hooks/use-nodes-dimensions";
 
 export type ViewModelParams = {
   setViewState: Dispatch<SetStateAction<ViewState>>;
   nodesModel: NodesModel;
   canvasRect: CanvasRect | undefined;
-  nodesRects: NodesRectsMap;
+  nodesDimensions: NodesDimensionsMap;
 };
