de410d247635368466663b618143e71fefb8a8d3
MaratFa <evolution99@rambler.ru>
2025-10-23 06:55:01 +0300

    edited

--- src/features/board/view-model/variants/idle.ts
+++ src/features/board/view-model/variants/idle.ts
@@ -6,6 +6,7 @@ import { ViewModel } from "../view-model-type";
 import { goToAddSticker } from "./add-sticker";
 import { goToSelectionWindow } from "./selection-window";
 import { Selection } from "../../domain/selection";
+import { goToEditSticker } from "./edit-sticker";
 
 export type IdleViewState = {
   type: "idle";
@@ -45,6 +46,16 @@ export function useIdleViewModel({
       ...node,
       isSelected: idleState.selectedIds.has(node.id),
       onClick: (e) => {
+        if (
+          idleState.selectedIds.size === 1 &&
+          idleState.selectedIds.has(node.id) &&
+          !e.ctrlKey &&
+          !e.shiftKey
+        ) {
+          setViewState(goToEditSticker(node.id));
+          return;
+        }
+
         if (e.ctrlKey || e.shiftKey) {
           select(idleState, [node.id], "toggle");
         } else {
@@ -54,6 +65,17 @@ export function useIdleViewModel({
     })),
     layot: {
       onKeyDown: (e) => {
+        if (
+          !e.shiftKey &&
+          !e.altKey &&
+          !e.metaKey &&
+          !e.ctrlKey &&
+          idleState.selectedIds.size === 1
+        ) {
+          const [id] = idleState.selectedIds.values();
+          setViewState(goToEditSticker(id));
+          return;
+        }
         if (e.key === "s") {
           setViewState(goToAddSticker());
         }
