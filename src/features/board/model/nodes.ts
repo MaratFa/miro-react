ba4a65f9b45ecdfad2007856b048c847492c1ab0
MaratFa <evolution99@rambler.ru>
2025-10-24 07:02:46 +0300

    edited

--- src/features/board/model/nodes.ts
+++ src/features/board/model/nodes.ts
@@ -43,6 +43,12 @@ export function useNodes() {
     ]);
   };
 
+  const updateStickerText = (id: string, text: string) => {
+    setNodes((lastNodes) =>
+      lastNodes.map((node) => (node.id === id ? { ...node, text } : node))
+    );
+  };
+
   const deleteNodes = (ids: string[]) => {
     setNodes((lastNodes) => lastNodes.filter((node) => !ids.includes(node.id)));
   };
