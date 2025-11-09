c97842d6f17a658e7570d6c14f29111ff17f3336
MaratFa <evolution99@rambler.ru>
2025-11-10 00:08:12 +0300

    edited

--- src/features/board/view-model/variants/idle/index.ts
+++ src/features/board/view-model/variants/idle/index.ts
@@ -1,5 +1,4 @@
 import { ViewModelParams } from "../../view-model-params";
-
 import { ViewModel } from "../../view-model-type";
 import { Selection } from "../../../domain/selection";
 import { useSelection } from "./use-selection";
@@ -8,14 +7,23 @@ import { useGoToEditSticker } from "./use-go-to-edit-sticker";
 import { useGoToAddSticker } from "./use-go-to-add-sticker";
 import { useMouseDown } from "./use-mouse-down";
 import { useGoToSelectionWindow } from "./use-go-to-selection-window";
+import { useGoToNodesDragging } from "./use-go-to-nodes-dragging";
 
 export type IdleViewState = {
   type: "idle";
   selectedIds: Set<string>;
-  mouseDown?: {
-    x: number;
-    y: number;
-  };
+  mouseDown?:
+    | {
+        type: "overlay";
+        x: number;
+        y: number;
+      }
+    | {
+        type: "node";
+        x: number;
+        y: number;
+        nodeId: string;
+      };
 };
 
 export function useIdleViewModel(params: ViewModelParams) {
@@ -27,18 +35,20 @@ export function useIdleViewModel(params: ViewModelParams) {
   const goToSelectionWindow = useGoToSelectionWindow(params);
   const mouseDown = useMouseDown(params);
   const selection = useSelection(params);
+  const goToNodesDragging = useGoToNodesDragging(params);
 
   return (idleState: IdleViewState): ViewModel => ({
     nodes: nodesModel.nodes.map((node) => ({
       ...node,
       isSelected: selection.isSelected(idleState, node.id),
-      onClick: (e) => {
+      onMouseDown: (e) => mouseDown.handleNodeMouseDown(idleState, node.id, e),
+      onMouseUp: (e) => {
+        if (!mouseDown.getIsStickerMouseDown(idleState, node.id)) return;
         const clickResult = goToEditSticker.handleNodeClick(
           idleState,
           node.id,
           e
         );
-
         if (clickResult.preventNext) return;
         selection.handleNodeClick(idleState, node.id, e);
       },
@@ -54,12 +64,13 @@ export function useIdleViewModel(params: ViewModelParams) {
     },
     overlay: {
       onMouseDown: (e) => mouseDown.handleOverlayMouseDown(idleState, e),
-
       onMouseUp: () => selection.handleOverlayMouseUp(idleState),
     },
     window: {
-      onMouseMove: (e) =>
-        goToSelectionWindow.handleWindowMouseMove(idleState, e),
+      onMouseMove: (e) => {
+        goToNodesDragging.handleWindowMouseMove(idleState, e);
+        goToSelectionWindow.handleWindowMouseMove(idleState, e);
+      },
       onMouseUp: () => mouseDown.handleWindowMouseUp(idleState),
     },
     actions: {
