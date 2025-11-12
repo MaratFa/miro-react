f6ef5391cea3a2ea1bb61b7a03ad46f246894b3b
MaratFa <evolution99@rambler.ru>
2025-11-13 01:29:05 +0300

    added

---
+++ src/features/board/ui/nodes/arrow.tsx
@@ -0,0 +1,56 @@
+import { Ref } from "react";
+import { Point, vectorFromPoints } from "../../domain/points";
+
+export function Arrow({
+  start,
+  end,
+  ref,
+  onClick,
+  onMouseDown,
+  omMouseUp,
+}: {
+  start: Point;
+  end: Point;
+  ref: Ref<SVGPathElement>;
+  onClick?: (e: React.MouseEvent<SVGPathElement>) => void;
+  onMouseDown?: (e: React.MouseEvent<SVGPathElement>) => void;
+  omMouseUp?: (e: React.MouseEvent<SVGPathElement>) => void;
+}) {
+  const diff = vectorFromPoints(start, end);
+  const angle = Math.atan2(diff.y, diff.x);
+  const arrowRightAngle = angle + Math.PI * (1 - 1 / 6);
+  const arrowLeftAngle = angle - Math.PI * (1 - 1 / 6);
+  const arrowRightDiff = [
+    Math.cos(arrowRightAngle) * 10,
+    Math.sin(arrowRightAngle) * 10,
+  ];
+  const arrowLeftDiff = [
+    Math.cos(arrowLeftAngle) * 10,
+    Math.sin(arrowLeftAngle) * 10,
+  ];
+
+  return (
+    <svg className="absolute left-0 top-0 pointer-events-none overflow-visible">
+      <path
+        className="pointer-events-auto "
+        stroke="black"
+        ref={ref}
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        onClick={onClick}
+        onMouseDown={onMouseDown}
+        onMouseUp={omMouseUp}
+        d={`
+          M ${start.x} ${start.y} L ${end.x} ${end.y}
+          M ${end.x} ${end.y} L ${end.x + arrowRightDiff[0]} ${
+          end.y + arrowRightDiff[1]
+        }
+          L ${end.x + -5 * Math.cos(angle)} ${end.y + -5 * Math.sin(angle)} 
+          L ${end.x + arrowLeftDiff[0]} ${end.y + arrowLeftDiff[1]}
+          L ${end.x} ${end.y}
+          `}
+      />
+    </svg>
+  );
+}
